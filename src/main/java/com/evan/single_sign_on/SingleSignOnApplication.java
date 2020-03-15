c15cd165737d9162b0fccc0a0d33321bc6a9f7ac
xu.yatao <xuyt1992@163.com>
2020-03-15 17:38:04 +0800

    first commit

---
+++ src/main/java/com/evan/single_sign_on/SingleSignOnApplication.java
@@ -0,0 +1,13 @@
+package com.evan.single_sign_on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSignOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleSignOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evan/single_sign_on/SingleSignOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evan.single_sign_on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSignOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
